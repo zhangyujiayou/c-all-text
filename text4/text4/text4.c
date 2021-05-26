f72322a5ae0804b76131fc605d62bdff2dfb1a9e
Your Name <123455@gdfg.com>
2021-05-27 01:45:13 +0800

    5。27的第一次修改

--- text4/text4/text4.c
+++ text4/text4/text4.c
@@ -30,30 +30,84 @@
 //else
 //语句3；
 
-int main()
-{
-	int age = 5;
-	if (age < 18)
-	{
-		printf("未成年\n");
-		printf("不能谈恋爱\n");
-	}
-	else
-	{
-		if (age >= 18 && age < 28)
-		    printf("青年\n");
-		else if (age >= 28 && age < 50)
-			printf("壮年\n");
-		else if (age >= 50 && age < 90)
-			printf("老年\n");
-		else if (age >= 90)
-			printf("老不死\n");
-	};
-	return 0;
-}
+//int main()
+//{
+//	int age = 5;
+//	if (age < 18)
+//	{
+//		printf("未成年\n");
+//		printf("不能谈恋爱\n");
+//	}
+//	else
+//	{
+//		if (age >= 18 && age < 28)
+//		    printf("青年\n");
+//		else if (age >= 28 && age < 50)
+//			printf("壮年\n");
+//		else if (age >= 50 && age < 90)
+//			printf("老年\n");
+//		else if (age >= 90)
+//			printf("老不死\n");
+//	};
+//	return 0;
+//}
 //由上面可以看出if else 控制多条语句时，多条语句需要用大括号括起来
 
 //C语言表示真假，0表示假，非0表示真；
 //注意---else是和离得最近的没有进行匹配的if进行匹配的 
 //列如将一个变量的类型去判断是不是一个常量时好的代码的书写风格是
 //5==num1
+
+//练习1：判断一个数是否为奇数（使用的核心思想是a%2==1？，也就是说看一个数除以2是不是余1）
+//练习2：输出1-100之间（出现多少到多少之间的数--使用的是利用循环来实现）的奇数（就是奇数的判别加上将要判别的a做成一个循环）
+//int main()
+//{
+//	int a = 1;
+//	/*scanf("%d", &a);*/
+//	for (a = 1; a < 101; a++)
+//	{
+//	if(1== a%2)
+//		printf("%d是奇数\n",a);
+//	}
+//	return 0;
+//} 
+
+
+//switch(整型变量)
+//{
+//    case 1:函数式
+//	  case 2:函数式
+//    case 3:函数式
+//	  case 4:函数式
+//}
+//整形变量里面是几就从几里面进去，一直向下执行，除非遇到break才会跳出switch语句
+//如果输入的整形变量超过了给出的case的数量，那么就会不执行函数，超出了处理的范围就不进行任何的工作
+//这个时候为了友好的提示输入人员，我们一般可以在末尾加上--default：函数式--在配上break进行友好的提示。
+
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)//n=1
+//	{
+//	case 1:
+//		m++;//m=3
+//	case 2:
+//		n++;//n=2
+//	case 3:
+//		switch (n)
+//		{//switch函数允许嵌套使用
+//		case 1:
+//			n++;
+//		case 2:
+//			m++, n++;//m=4,n=3
+//			break;
+//		}
+//	case 4:
+//		m++;//m=5，n=3
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("m=%d,n=%d\n", m, n);//m=5，n=3
+//}
